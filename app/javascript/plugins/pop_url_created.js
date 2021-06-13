594e21c6a54d6547dbca3eb0f7ea5eb8e0353e07
auriane gregoire <auriane.gregoire@gmail.com>
2021-06-13 17:14:21 +0200

    pop up short url created and copy btn js

--- app/javascript/plugins/pop_url_created.js
+++ app/javascript/plugins/pop_url_created.js
@@ -1,14 +1,23 @@
 const popUrlCreated= () => {
-  const close = document.querySelector('.fa-times')
-  const pop = document.getElementById('pop')
+  const close = document.querySelector('.fa-times');
+  const pop = document.getElementById('pop');
+  const url = document.getElementById('pop-short-url').innerText;
+  const copy = document.getElementById('copy-btn')
 
   // Do not execute the validation if you are not in the new form page :
   if (!pop) { return };
 
 
-  close.addEventListener('click', () => {
+close.addEventListener('click', () => {
     pop.style.display = "none";
   });
+
+
+copy.addEventListener('click', () => {
+  navigator.clipboard.writeText(url);
+  copy.innerText = "COPIED !"
+});
 }
+
 
 export { popUrlCreated }
